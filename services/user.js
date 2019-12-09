f59d4f45ec8765a496a70a63ad0fd6b4dcc209a6
claudio.uslenghi <claudio.uslenghi@gmail.com>
2019-12-09 11:41:45 -0300

    remove .env file and .env.example

--- services/user.js
+++ services/user.js
@@ -15,7 +15,7 @@ exports.login = async (email, password) => {
   const jwtToken = JWT.sign({ email });
   const user = await persistence.getUser(email);
 
-  logger.info(`3 login ${jwtToken}`);
+  logger.info(`jwtToken= ${jwtToken}`);
   if (user === null || password !== user[0].password) throw new customError('wrong user or password');
 
   return {
@@ -33,15 +33,12 @@ exports.login = async (email, password) => {
  */
 exports.add = async (user) => {
 
-  logger.info(`service${JSON.stringify(user)}`);
+  logger.info(`add user, user= ${JSON.stringify(user)}`);
 
   const existUser = await persistence.getUser(user.email);
   if (existUser !== null) throw new customError('User already exist');
 
-  logger.info('1 service');
   const result = await persistence.addUser(user);
 
-
-  logger.info('2 service');
   return result;
 };
